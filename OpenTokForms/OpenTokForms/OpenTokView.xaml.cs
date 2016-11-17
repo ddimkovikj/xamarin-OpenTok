491a3cc4026e8d2b421e046c3d690e7e454a91fc
dragan.dimkovikj <dragan.dimkovikj@haselt.com>
2016-11-17 18:25:32 +0100

    Initial commit

---
+++ OpenTokForms/OpenTokForms/OpenTokView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace OpenTokForms
+{
+	public partial class OpenTokView : View
+	{
+		public OpenTokView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
